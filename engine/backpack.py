6f6c78ef65718233a6a460f7f3feccc93e1c5299
WilsonVinson <cprewilsonvinson@gmail.com>
2022-09-13 17:32:53 +0800

    main

--- engine/backpack.py
+++ engine/backpack.py
@@ -1,6 +1,19 @@
 
+import pygame
+
 import engine.object as object
 
 class Backpack:
-    def __init__(self):
+    def __init__(self,item_size):
+        self.item_size = item_size
+        
+        self.window_size = pygame.display.get_surface().get_size()
+        
+    def renderer(self,surface):
+        pass
+    
+    def motion(self):
+        pass
+    
+    def touch(self):
         pass
--- engine/object/item.py
+++ engine/object/item.py
@@ -4,5 +4,53 @@ import pygame
 import drivers
 
 class Item:
-    def __init__(self):
-        pass
+    def __init__(self,id,pos,size):
+        self.id = id
+        self.pos = pos
+        self.size = size
+        
+        self.num = 1
+        
+        self.translucence = False
+        
+        yaml = drivers.yaml.yaml_driver.YamlDriver()
+        self.config = yaml.read(read_file='data/engine/item.yml')
+        
+        self.assets = pygame.image.load(self.config['assets_original']+str(self.id)+'.png').convert_alpha()
+        
+        self.rect = self.assets.get_rect()
+        self.width = self.rect.width
+        
+    def renderer(self,surface):
+        if self.translucence == True:
+            self.assets.set_alpha(48)
+        if self.translucence == False:
+            self.assets.set_alpha(255)
+            
+        surface.blit(self.assets, self.rect)
+    
+    def motion(self):
+        self.motioning = False
+        try:
+            if pygame.mouse.get_pressed()[0] == True:
+                if pygame.Rect.collidepoint(self.rect,pygame.mouse.get_pos()):
+                    self.motioning = True
+                    # pygame.mixer.Channel(1).play(pygame.mixer.Sound("assets/sound/effect/switch_001.ogg"))
+                    # time.sleep(0.1)
+        except:
+            pass
+        
+        return self.motioning
+    
+    def touch(self):
+        self.clicking = False
+        try:
+            if pygame.mouse.get_pressed()[0] == True:
+                if pygame.Rect.collidepoint(self.rect,pygame.mouse.get_pos()):
+                    self.clicking = True
+                    # pygame.mixer.Channel(1).play(pygame.mixer.Sound("assets/sound/effect/switch_001.ogg"))
+                    # time.sleep(0.1)
+        except:
+            pass
+        
+        return self.clicking
